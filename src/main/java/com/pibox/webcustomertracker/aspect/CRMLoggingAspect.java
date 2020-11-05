9aac964ae261f5be77ee7a53ca9defa1f121f946
Valentin Piho <val.piho@gmail.com>
2020-11-05 18:00:46 +0200

    AspectJ library added. Configuration file

---
+++ src/main/java/com/pibox/webcustomertracker/aspect/CRMLoggingAspect.java
@@ -0,0 +1,11 @@
+package com.pibox.webcustomertracker.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+
+}
